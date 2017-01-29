14804f0c3c8de77700e2a9add8d0900b9e119629
bonkstok <johnnyboythabest@hotmail.com>
2017-01-29 10:03:44 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,12 @@
 node 'puppetmaster.jvv.local'{
 class{'r10k':
   remote => "git@github.com:puppet-presentation/control.git",
-}}
+}
+
+node default{
+notify{"Sorry, no role found for host ${::fqdn}.:}
+}
+
+
+
+}
