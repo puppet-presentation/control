9a0f50624c880ba85ef8f9cf123686f4779fcd0f
bonkstok <johnnyboythabest@hotmail.com>
2017-01-29 16:56:22 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,9 @@ class{'r10k':
   remote => "git@github.com:puppet-presentation/control.git",
 }}
 
+node 'webserver01'{
+  include roles::webserver
+}
 node default{
 notify{"Sorry, no role found for host:${::trusted['hostname']} in environment ${::environment}":}
 }
