aa7930eac6d1f708bd91d6da79af14e975a7f7df
bonkstok <johnnyboythabest@hotmail.com>
2017-01-29 21:23:36 +0100

    Delete site.pp

--- manifests/site.pp
+++
@@ -1,8 +0,0 @@
-node 'puppetmaster.jvv.local'{
-class{'r10k':
-  remote => "git@github.com:puppet-presentation/control.git",
-}}
-
-node default{
-notify{"Sorry, no role found for host:${::trusted['hostname']} in environment ${::environment}":}
-}
